3087e8ce7d835a4ea94751348c9d0da3e5bc0ea4
CJHCSM <108597918+CJHCSM@users.noreply.github.com>
2024-01-31 00:01:04 +0800

    Update defaults.h

--- trunk/user/shared/src/defaults.h
+++ trunk/user/shared/src/defaults.h
@@ -30,7 +30,7 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
-#define DEF_LAN_ADDR		"192.168.2.2"
+#define DEF_LAN_ADDR		"192.168.2.3"
 #define DEF_LAN_DHCP_BEG	"192.168.2.50"
 #define DEF_LAN_DHCP_END	"192.168.2.244"
 #define DEF_LAN_MASK		"255.255.255.0"
